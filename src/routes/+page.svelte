399b5c883037d296478d1e1e8337243495032840
GeorgeRNG <grogrng@gmail.com>
2023-11-29 16:43:56 +0000

    what possessed me to use var, it caused a warn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,9 @@
     import Template from "$lib/Template.svelte";
     import { ActionDump, Template as CodeTemplate } from "df.ts";
 
-    var stack = true;
-    var openableChests = true;
-    var template = CodeTemplate.parse({"blocks":[
+    let stack = true;
+    let openableChests = true;
+    let template = CodeTemplate.parse({"blocks":[
         {"id":"block","block":"func","args":{"items":[{"item":{"id":"pn_el","data":{"name":"name","type":"any","default_value":{"id":"num","data":{"name":"0"}},"plural":false,"optional":true}},"slot":0},{"item":{"id":"pn_el","data":{"name":"name","type":"var","plural":true,"optional":false}},"slot":1},{"item":{"id":"pn_el","data":{"name":"name","type":"dict","plural":true,"optional":true}},"slot":2},{"item":{"id":"pn_el","data":{"name":"name","type":"list","plural":false,"optional":false}},"slot":3},{"item":{"id":"pn_el","data":{"name":"name","type":"vec","plural":false,"optional":false,"description":"hi&ahi<green>hi","note":"&ahi<green>hi"}},"slot":4},{"item":{"id":"hint","data":{"id":"function"}},"slot":25},{"item":{"id":"bl_tag","data":{"option":"False","tag":"Is Hidden","action":"dynamic","block":"func"}},"slot":26}]},"data":""},
         {"id":"block","block":"player_action","args":{"items":[{"item":{"id":"txt","data":{"name":"&astring! %default"}},"slot":0}]},"action":""},
         {"id":"block","block":"player_action","args":{"items":[{"item":{"id":"comp","data":{"name":"<green>hi"}},"slot":0}]},"action":""},
@@ -18,12 +18,12 @@
         {"id":"block","block":"player_action","args":{"items":[{"item":{"id":"var","data":{"name":"hi","scope":"unsaved"}},"slot":0},{"item":{"id":"var","data":{"name":"hello","scope":"saved"}},"slot":1},{"item":{"id":"var","data":{"name":"morning","scope":"local"}},"slot":2},{"item":{"id":"var","data":{"name":"night","scope":"line"}},"slot":3}]},"action":""},
         {"id":"block","block":"player_action","args":{"items":[{"item":{"id":"g_val","data":{"type":"Player Count","target":"Default"}},"slot":0},{"item":{"id":"g_val","data":{"type":"Current Health","target":"Default"}},"slot":1}]},"action":""}]
         });
-    var selection = new Selection(0);
-    var actiondump : ActionDump;
+    let selection = new Selection(0);
+    let actiondump : ActionDump;
 
-    var blockSize = "10em";
-    var tooltipScale = "2.5";
-    var slotSize = "3em";
+    let blockSize = "10em";
+    let tooltipScale = "2.5";
+    let slotSize = "3em";
 
     async function db(event: Event) {
         actiondump = ActionDump.parse(JSON.parse(await ((event.target as HTMLInputElement).files ?? [])[0].text()));
