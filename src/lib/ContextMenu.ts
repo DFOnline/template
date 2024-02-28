f3719fbda3790891f516261e5d3b2b5a7edff99c
GeorgeRNG <81434111+GeorgeRNG@users.noreply.github.com>
2024-02-28 20:15:19 +0000

    add and test abstract context menus

---
+++ src/lib/ContextMenu.ts
@@ -0,0 +1,53 @@
+export class ContextMenu {
+    items: ContextButton<any>[]
+
+    constructor(...items: ContextButton<any>[]) {
+        this.items = items;
+    }
+}
+
+interface ContextButtonTypes {
+    "button": string
+    "checkbox": boolean
+    "input": string
+    "menu": ContextMenu
+}
+export class ContextButton<T extends keyof ContextButtonTypes> {
+    type: T
+    label: ContextButtonTypes[T]
+    value?: any
+    update: (value: ContextButtonTypes[T]) => void
+
+    constructor(type: T, label: ContextButtonTypes[T], update: (value: ContextButtonTypes[T]) => void, value?: any) {
+        this.type = type;
+        this.label = label;
+        this.update = update;
+        this.value = value;
+    }
+}
+
+export function combineContextMenus(...menus: ContextMenu[]): ContextMenu {
+    const buttons: ContextButton<any>[] = [null as unknown as ContextButton<any>];
+    menus.forEach(menu => {
+        if(buttons[0] == null) {
+            buttons.pop();
+            buttons.push(...menu.items)
+            return;
+        }
+        let didPair = false;
+        const buttonsToRemove: number[] = [];
+        buttons.forEach((button,i) => {
+            menu.items.forEach(item => {
+                if(button.label != item.label) return;
+                didPair = true;
+                if(button.type != item.type) {buttonsToRemove.push(i); return;}
+                if(button.value != item.value) buttons[i].value = undefined;
+                const update = buttons[i].update;
+                buttons[i].update = value => {update(value); item.update(value); }
+            });
+            if(!didPair) buttonsToRemove.push(i);
+        });
+        new Set(buttonsToRemove.sort((a,b) => b-a)).forEach(i => buttons.splice(i,1));
+    });
+    return new ContextMenu(...buttons);
+}
---
+++ src/lib/__test__/ContextMenu.test.ts
@@ -0,0 +1,45 @@
+import { describe, test, expect, mock } from 'bun:test'
+import { ContextButton, ContextMenu, combineContextMenus } from '$lib/ContextMenu.js'
+
+describe("Context Menus", () => {
+    const n = () => {}
+    test("Common Buttons", () => {
+        const buttonA = new ContextButton('button','buttonA',n);
+        const buttonB = new ContextButton('button','buttonB',n);
+        const buttonC = new ContextButton('button','buttonC',n);
+
+        const A = new ContextMenu(buttonA,buttonB);
+        const B = new ContextMenu(buttonB,buttonC);
+
+        const expectedResult = new ContextMenu(buttonB);
+        
+        expect(combineContextMenus(A,B)).toStrictEqual(expectedResult)
+    });
+    test("Combine Values", () => {
+        const inputA1 = new ContextButton('input','inputA',n,0);
+        const inputB1 = new ContextButton('input','inputB',n,1);
+        const inputB2 = new ContextButton('input','inputB',n,2);
+
+        const A = new ContextMenu(inputA1,inputB1);
+        const B = new ContextMenu(inputA1,inputB2);
+
+        const expectedB = new ContextButton('input','inputB',n,undefined);
+        const expectedResult = new ContextMenu(inputA1,expectedB);
+
+        expect(combineContextMenus(A,B).items.map(i => [i.label,i.type,i.value])).toEqual(expectedResult.items.map(i => [i.label,i.type,i.value]));
+    });
+    test("All Callbacks", () => {
+        const mockA = mock();
+        const mockB = mock();
+        const versionA = new ContextButton('button','',mockA);
+        const versionB = new ContextButton('button','',mockB);
+
+        const A = new ContextMenu(versionA);
+        const B = new ContextMenu(versionB);
+
+        combineContextMenus(A,B).items[0].update(undefined);
+
+        expect(mockA).toHaveBeenCalled();
+        expect(mockB).toHaveBeenCalled();
+    })
+})
